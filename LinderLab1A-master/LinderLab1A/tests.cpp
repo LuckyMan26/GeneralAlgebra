ec894196052c385039506d0d90caf39162a1c30e
Vyacheslav <slavixg409@gmail.com>
2023-04-29 14:02:34 +0300

    (abs(base) == mod) fix

--- LinderLab1A-master/LinderLab1A/tests.cpp
+++ LinderLab1A-master/LinderLab1A/tests.cpp
@@ -178,6 +178,12 @@ TEST_CASE("Finite numbers") {
 
 	fin1 = FiniteNumber("-10 x6");
 	CHECK(fin1.toString() == "2");
+
+	fin1 = FiniteNumber("-6 x6");
+	CHECK(fin1.toString() == "0");
+
+	fin1 = FiniteNumber("-6 x5");
+	CHECK(fin1.toString() == "4");
 }
 
 //Tests by M. Tyshchenko
