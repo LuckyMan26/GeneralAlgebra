8a59eadc1fb27c3c81eee8d01f59e17297381ae3
Dm <dmytromk@gmail.com>
2023-04-29 22:01:12 +0300

    small refactoring for Miller-Rabin

--- LinderLab1A-master/LinderLab1A/MillerRabin.h
+++ LinderLab1A-master/LinderLab1A/MillerRabin.h
@@ -1,74 +1,75 @@
 #pragma once
 #include <vector>
 
-// Wrapper around miller_rabin_do
+
+/*
+	Implemented by Dmytro Mandziuk and Oleksii Onishchenko
+*/
 template <typename NumberType>
-bool miller_rabin(NumberType num, int max_iterations = 4)
-{
-	static NumberType zero = NumberType(0), one = NumberType(1), two = NumberType(2), four = NumberType(4);
-
-	if (num < two || num == four)  return false;
-	if (num < four) return true;
-
-	// Find r such that  num - 1 = (2^d) * r  and r is odd
-	NumberType d = num - one;
-	while (d % two == zero)
-	{
-		d = d / two;
-	}
-
-	for (int i = 0; i < max_iterations; i++)
-		if (!miller_rabin_do(d, num))
-			return false;
-
-	return true;
-}
-
-// (x^y) % p
-template <typename NumberType>
-NumberType power(NumberType x, NumberType y, NumberType p)
+NumberType mod_power(NumberType base, NumberType exp, NumberType mod)
 {
 	NumberType res = NumberType(1);
-	static NumberType zero = NumberType(0), two = NumberType(2);
-	x = x % p;
+	base = base % mod;
 
-	while (y > zero)
+	while (exp > NumberType(0))
 	{
 		// If y is odd, multiply x with result
-		if (y % two != zero)
-			res = (res * x) % p;
+		if (exp % NumberType(2) != NumberType(0))
+			res = (res * base) % mod;
 
 		// y must be even now
-		y = y / two; // y = y/2
-		x = (x * x) % p;
+		exp = exp / NumberType(2); // y = y/2
+		base = (base * base) % mod;
 	}
 
 	return res;
 }
 
-
-template <typename NumberType>
-bool miller_rabin_do(NumberType r, NumberType n)
-{
-	static NumberType one = NumberType(1), two = NumberType(2);
-
-	// a = random number from [2, n-2]
-	NumberType random_num = NumberType(2) + two % (n - NumberType(4));
-
-	// y = (random_num ^ r) mod n
-	NumberType y = power(random_num, r, n);
-
-	while (r != n - one)
+namespace MillerRabin {
+	// (base^exp) % mod
+	template <typename NumberType>
+	bool miller_rabin_do(NumberType r, NumberType n)
 	{
-		y = (y * y) % n;
-		r *= two;
+		// a = random number from [2, n-2]
+		NumberType random_num = NumberType(2) + NumberType(rand()) % (n - NumberType(4));
 
-		NumberType temp = NumberType(n - 1);
+		// y = (random_num ^ r) mod n
+		NumberType y = mod_power(random_num, r, n);
 
-		if (y == one)      return false;
-		if (y == temp)    return true;
+		if (y == 1 || y == n - 1)
+			return true;
+
+		while (r != n - NumberType(1))
+		{
+			y = (y * y) % n;
+			r *= NumberType(2);
+
+			if (y == NumberType(1))      return false;
+			if (y == NumberType(n - 1))    return true;
+		}
+
+		// Return composite
+		return false;
 	}
 
-	// Return composite
-	return false;
+	// Wrapper around miller_rabin_do
+	template <typename NumberType>
+	bool miller_rabin(NumberType num, int max_iterations = 4)
+	{
+		if (num <= NumberType(1) || num == NumberType(4))  return false;
+		if (num <= NumberType(3)) return true;
+
+		// Find r such that  num - 1 = (2^d) * r  and r is odd
+		NumberType d = num - NumberType(1);
+		while (d % NumberType(2) == NumberType(0))
+		{
+			d = d / NumberType(2);
+		}
+
+		for (int i = 0; i < max_iterations; i++)
+			if (!miller_rabin_do(d, num))
+				return false;
+
+		return true;
+	}
 }
