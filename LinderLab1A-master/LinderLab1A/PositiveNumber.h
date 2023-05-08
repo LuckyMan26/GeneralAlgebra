70d395e11f26457c947d1960c5aab0d2c19e8230
Artem Volyk <volikartem6@gmail.com>
2023-05-08 22:02:15 +0300

    Fixe of GCD(PositiveNumber,PositiveNumber)

--- LinderLab1A-master/LinderLab1A/PositiveNumber.h
+++ LinderLab1A-master/LinderLab1A/PositiveNumber.h
@@ -392,17 +392,8 @@ public:
 	static PositiveNumber GCD(PositiveNumber n1, PositiveNumber n2) {
 		if (n1 == 0)
 			return n2;
-		if (n2 == 0)
-			return n1;
-		while (n1 != n2) {
-			if (n1 > n2) {
-				n1 -= n2;
-			}
-			else {
-				n2 -= n1;
-			}
-		}
-		return n1;
+
+		return GCD(n2 % n1, n1);
 	}
 	//Implemented by A.Volyk
 	static PositiveNumber LCM(PositiveNumber n1, PositiveNumber n2) {
